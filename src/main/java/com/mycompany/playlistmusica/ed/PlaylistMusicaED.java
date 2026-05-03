f8ed83729e45198bb4dcd6507f71d870685f2808
Anna Braga <annaclarabraga2020@gmail.com>
2026-05-03 13:24:43 -0300

    inicio - teste de conexão

---
+++ src/main/java/com/mycompany/playlistmusica/ed/PlaylistMusicaED.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.playlistmusica.ed;
+
+/**
+ *
+ * @author annac
+ */
+public class PlaylistMusicaED {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
